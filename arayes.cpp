e92eaf0de6027b72b11d2c168f1d81dbb929d207
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-22 15:29:26 +0300

    ex23: double free detected in tcache; Aborted ( core dumped )

--- arayes.cpp
+++ arayes.cpp
@@ -1,36 +1,12 @@
 #include<iostream>
-
-void f(int* a){
-	std::cout << "hi" << '\n';
-}
-
-/*
- void f(int a[3]){
-	std::cout << "hii" << '\n';
-} no, this redifinition
-*/
+#include<vector>
 
 int main(){
     
-    int a[5]{};
-    a[1] = 1;
+	int* a = new int[100];
+	delete[] a; 
 
-    for(int i = 0; i < 5 ;++i){
-	    std::cout << a[i];
-    }
-
-    std::cout << *a << '\n';
-    std::cout << *(a + 3) << '\n'; //array to pointer convertion
-
-    int* p = a + 3;
-    std::cout << p[-1] << '\n'; //Ok, equivalent to *(p-1)
-
-    //a[2] == *(a + 2)
-    std::cout << a[2] << ' ' << 2[a] << '\n';
-
-    // a != int*
-    // ++a , no, this error
-
-    std::cout << sizeof(a) << ' ' << sizeof(p) << '\n';
+	std::vector<int> v(10);
+	delete &v[0];
 }
 
