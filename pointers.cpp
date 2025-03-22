06914b73bbb30924e3a3f4314277cb80d7f4ec99
Jiber <anton.zhebrikov@math.msu.ru>
2025-03-22 12:25:22 +0300

    ex18: передеча переменных в функцию, адрес мертвых переменных

--- pointers.cpp
+++ pointers.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 #include<vector>
 
+void swap(int* x, int* y){
+	int t = *x;
+	*x = *y;
+	*y = t;
+}
+
 int main(){
+    int a = 0;
+    int* p = &a;
+    {
+         int b = 2;
+	 p = &b;
+    }
+
+    std::cout << p << '\n';
+    std::cout << *p << '\n'; // UB, but must likely 2
+
     int x = 0;
-    int* px = &x;//& : T -> T* Address
-
-    std::cout << &x << '\n';
-    std::cout << *px << '\n'; //*: T* -> T Dereference
+    int y = 1;
     
-    (px+1); // +: (T*, int) -> T*
-    
-    std::vector<int> v = {1,2,3,4,5};
-    int* pv = &v[0];
-
-    std::cout << *pv << '\n';
-    std::cout << *++pv << '\n';
+    swap(&x, &y);
+    std::cout << x << '\n';
+    std::cout << y << '\n';
 
 }
