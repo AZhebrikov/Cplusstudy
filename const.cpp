4c74e31db082bf35f6198b35b02c5dec3518b0e2
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-23 16:56:47 +0300

    ex34: еще пара примеров на проблемы с const

--- const.cpp
+++ const.cpp
@@ -2,19 +2,7 @@
 
 int main(){
 
-	int x = 5;
-	const int& r = x;
-	++x; // Ok
+	const int* p = new const int[10]; // CE, так как не проинициализировали данные.
 	
-	r = 5;//CE
-	++r; //CE
-	
-	int& const cr = x; //CE, так как ссылка по природе неизменна.
-	
-	int& r2 = r;//CE
-
-	const int y = 0;
-	int& y2 = y;//CE
-	// const потерять нельзя нигде по дороге.
-
+	int* p = new const int[10]{} // CE, нельзя так как забывается константность.
 }
