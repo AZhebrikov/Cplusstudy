8d500d78f4c55aaeae7c1441c3ce92cc106e1136
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-24 10:32:48 +0300

    ex3: ODR

--- dec_dif_sc.cpp
+++ dec_dif_sc.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 
-namespace N{
-    int x = 0;
-}
+// One Definition Rule (ODR)
+void f(int x);
+void f(int x);
+void f(int x);
+//but one
+void f(int x){}
+// Нужно для перекрестных функций 
 
-namespace NN{
-    int x = 1;
-}
+void f(){}// перегрузка функций 
+
+
+void fun(int x){}
+void fun(float x){}
+
 
 int main(){
-    // using N::x;
-    // int x = 1; Ошибка с повторным объявлением.
-
-    using namespace N;
-    using namespace NN;
-
-    std::cout << x ;
+    fun(1.3)//CE так как конвертация double одинаково плоха как в float и int 
 }
+
+
