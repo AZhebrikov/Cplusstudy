fbba90ca49cf8914a389afcd29805dec7813dc1c
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-24 10:26:31 +0300

    ex2: reference to 'x' is ambiguous

--- dec_dif_sc.cpp
+++ dec_dif_sc.cpp
@@ -1,26 +1,19 @@
 #include<iostream>
 
-int a; //global scope
-
-//declaration
-void f(int x);
-class S;
-struct U;
-
-
-namespace N {
-    int x;
+namespace N{
+    int x = 0;
 }
 
-namespace N {
-    int y;
+namespace NN{
+    int x = 1;
 }
-
-using namespace N; //Принесение всего содержимого пространства имен N, плохой стиль.
-using N::x;//Принесение конкретной функции из пространства имен, нормально.
 
 int main(){
-    using vi = std::vector<int>; // это не объявление нового типа, а просто алиас для него.
-    // Преимущество по сравнению с typedef  с тем, что using более умная функция, и он 
-    // более читабельный.
+    // using N::x;
+    // int x = 1; Ошибка с повторным объявлением.
+
+    using namespace N;
+    using namespace NN;
+
+    std::cout << x ;
 }
