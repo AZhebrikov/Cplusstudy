200bc02e262fe17109c2074d5d11c3b849295870
Jiber <anton.zhebrikov@math.msu.ru>
2025-03-24 17:40:10 +0300

    ex41: пример на private и public поля

--- private.cpp
+++ private.cpp
@@ -1,23 +1,21 @@
 #include<iostream>
 
-class C{ 
-	int x{5};
+class C{
+private:
+	class Inner {
+	public:
+		int x{1};
+	private:
+		int y{2};
+	};
+
 public:
-	void f(int y){
-		std::cout << x + y;
+	Inner f(){
+		return Inner();
 	}
-
-	friend void g(C, int);
-	// Забавный момент, написав friend мы можем написать следом и определение функции, однако это будет не метод, 
-	// а внешняя функция, поэтому ее же повторение вне блока будет redifinition.
-	friend class CC;
 };
-
-void g(C c, int y){
-	std::cout << c.x + y + 1 << '\n';
-}
 
 int main(){
 	C c;
-	g(c,2); //без friend не получится, так как х является приватной переменной.
+	std::cout << c.f().x << '\n'; // CE or 1? -> 1
 }
