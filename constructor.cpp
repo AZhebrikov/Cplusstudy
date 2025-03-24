f2bf20a5faee5deea4fb19df5f4de3fd71d18b10
Jiber <anton.zhebrikov@math.msu.ru>
2025-03-25 00:06:13 +0300

    ex45: default constructor

--- constructor.cpp
+++ constructor.cpp
@@ -6,29 +6,24 @@ class String{
 	size_t sz = 0;
 	size_t cap = 0;
 public:
-	String(){} //default constructor
-	
+	//String(){} - default constructor
+	// если в классе будет реализован хоть один конструктор, то компилятор не 
+	// будет создавать конструктор по умолчанию, поэтому мы его явно написали.
+	// Однако мы можем попросить конструктор это сделать.
+	String() = default;
+	// явно объявленный и неявно определенный
+
 	String(size_t n, char c): arr(new char[n+1]), sz(n), cap(n+1){
 		std::memset(arr, c, n);
 		arr[sz] = '\0';
 		std::cout << 1 << '\n';
-	}// Пример нетривиального конструктора.
-	
-	String(std::initializer_list<char> list)
-			: arr(new char[list.size() + 1])
-			, sz(list.size())
-			, cap(sz + 1) {
-	 	std::copy(list.begin(), list.end(), arr);
-		arr[sz]='\0';
-		std::cout << 2 << '\n';
 	}
+	
 };
 
 int main(){
 
 	String v; //default initialization
 	
-	String s1{2, 'a'};
-	String s2{'a', 'b', 'c', 'd', 'f'};
 }
 
