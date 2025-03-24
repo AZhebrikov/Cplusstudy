2d271b4ee5e33e021080c2187880a7b747c5559b
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-24 10:20:36 +0300

    ex0: базовые типы

---
+++ type.cpp
@@ -0,0 +1,20 @@
+// integral types
+
+(4) unsigned int / int [-2^31, 2^31 - 1]
+long 
+(8) long long [-2^63, 2^63 - 1]
+(2) short
+(1) signed char / char
+
+int8_t / uint8_t
+int16_t
+int32_t
+int64_t
+
+size_t == unsigned long long
+
+// floating point types
+
+(4) float
+(8) double
+(16) long double
