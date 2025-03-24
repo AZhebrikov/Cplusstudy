993089cffb7f82fdb68e8b68704f8591c67a9d63
AZhebrikov <anton.zhebrikov@math.msu.ru>
2025-03-24 10:45:37 +0300

    ex6: if, else, switch

---
+++ control.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main(){
+    if(/*declaration later C++17*/;/*bool-expression*/){
+
+    }else if(...){
+
+    }else{
+
+    }
+
+    switch(value_case){
+    case val1:
+        ...
+    case val2:
+        ...
+        break;
+    default:
+        ...
+    }
+}
