d715510d41b78ecc95332bd8c32b7a44c4185d47
Anton Zhebrikov <zhebrikov-av@172.31.101.218-vpn.dhcp.yndx.net>
2025-03-30 11:47:29 +0300

    ex61: тонкость с operator=

--- operators_overloading.cpp
+++ operators_overloading.cpp
@@ -8,35 +8,19 @@ struct Complex {
     Complex(double re): re(re) {}
     Complex(double re, double im): re(re), im(im) {}
 
-    //version 2
+    Complex& operator=(const Complex& other) & {
+        re = other.re;
+        im = other.im;
+        return *this;
+    }
+    
     Complex& operator+=(const Complex& other) {
         re += other.re;
         im += other.im;
         return *this;
     }
-
-    // version 1
-    // Complex& operator+=(const Complex& other){
-    //     *this = *this + other;
-    //     return *this;
-    // } Очень плохой код, работает очень не эффективно. Видно это на примере String: сначала будет создана
-    //   строка с помощью оператора суммы *this + other, затем эта строка будет скопирована в объект, что тоже
-    //   делается за линию, а уже после вернется ссылка на нее.
-
-    // version 1
-    // Complex operator+(const Complex& other) const {
-    //     return Complex{re + other.re, im + other.im};
-    //}
-    //  У такого подхода есть своя проблема, мы бы с вами хотели уметь складывать числа Complex с
-    //  double,  что мы сможем сделать. 
 };
 
-// version 2
-// Complex operator+(const Complex& a, const Complex& b){
-//     return Complex(a.re + b.re,  a.im + b.im);
-// }
-
-//version 3
 Complex operator+(const Complex& a, const Complex& b) {
     Complex result = a;
     result += b;
@@ -44,12 +28,15 @@ Complex operator+(const Complex& a, const Complex& b) {
 }
 
 int main() {
-    Complex c{1};
-    c + 3.14; // синтаксический сахар для c.operator+(3.14), от 3.14 возьмется неявный каст к Complex с помощью
-              // конструктора, и затем сложение.
-    3.14 + c; // но в такой реализации такое сложение уже не возможно. Компилятор не будет проверяться все возможные 
-              // конвертации double к каком-нибудь типу, у которого найдется operator+(const Complex&), а просто выведет 
-              // ошибку.
-              //Поэтому лучше все подобные операторы не делать методами класа.
+
+Complex a{1.0};
+Complex b{2.0};
+Complex d{3.0};
+a + b = d; // CE or OK? Вообще мы говорили, что для стандартных типов присваивать rvalue нельзя, но никто не говорил,
+           // что это правило сохраниться для наших типов. С этим можно бороться двумя способами:
+           // сделать возвращаемое значение const Complex, но этот вариант не самый хорошый, есть вариант, который пока
+           // нужно взять на веру. Написать свой оператор присваивания и указать, что он должен рботать только для
+           // lvalue типов.
 
 }
+    
